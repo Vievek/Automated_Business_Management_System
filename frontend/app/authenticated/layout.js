0a8b449950610394870a04492f3861e3934ca5ec
vievegan <vieveganthaneswaran@gmail.com>
2025-03-29 11:39:18 +0530

    feat:light mode dark mode toggle work

--- frontend/app/authenticated/layout.js
+++ frontend/app/authenticated/layout.js
@@ -4,9 +4,19 @@ import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import useAuthStore from "@/stores/authStore";
 import { SideNav } from "@/app/_components/SideNav";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function Layout({ children }) {
   const { user } = useAuthStore();
+  const { setTheme } = useTheme();
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
 
   if (!user) {
@@ -35,6 +45,28 @@ function Layout({ children }) {
               : "Worker"}{" "}
             Dashboard
           </div>
+        </div>
+        <div className="absolute right-4 top-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </header>
       {/* Side Navigation */}
--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/app/_components/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +20,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
         <Toaster />
       </body>
     </html>
